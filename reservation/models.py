6972befcb5da7986107ca26c6bbd3cb41f7fa4cd
Elisei <10thecrew01@gmail.com>
2024-01-13 17:28:31 +0100

    change db field

--- reservation/models.py
+++ reservation/models.py
@@ -17,8 +17,8 @@ TIME_ZONE = os.getenv('TIME_ZONE')
 class Appointment(models.Model):
     phone = models.CharField(max_length=32, default=None)
     telegram = models.CharField(max_length=64, null=True, blank=True)
-    start_date = models.DateTimeField(unique=True)
-    end_date = models.DateTimeField(unique=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
 
     @staticmethod
     def find_by_two_months():
