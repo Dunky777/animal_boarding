c74bdad7d069f345569662a42396cd6d5547995c
Elisei <10thecrew01@gmail.com>
2024-01-14 15:20:04 +0100

    add auth

--- animal_boarding/urls.py
+++ animal_boarding/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('reservation.urls')),
-    path('api/', include('staff.urls')),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
 ]
