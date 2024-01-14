efbe646b903428a25c437c740b835e804b23349c
Elisei <10thecrew01@gmail.com>
2024-01-14 23:29:47 +0100

    add token verification

--- staff/urls.py
+++ staff/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from .views import *
 
 urlpatterns = [
     path('obtain-pair', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh-token', TokenRefreshView.as_view(), name='token_refresh')
+    path('refresh-token', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify-token', TokenVerifyView.as_view(), name='token-verify')
 ]
