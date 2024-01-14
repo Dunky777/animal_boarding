2b021cafa33f4818dff5fc544b8bc1a76afb5f85
Elisei <10thecrew01@gmail.com>
2024-01-14 21:43:00 +0100

    add auth with jwt token

--- staff/urls.py
+++ staff/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import *
 
 urlpatterns = [
+    path('obtain-pair', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh-token', TokenRefreshView.as_view(), name='token_refresh')
 ]
